6ef9f33269a73a9cf5ef247b58d564f79b318f73
Yann Faussurier <100946859+YannFaussurier@users.noreply.github.com>
2022-11-28 12:45:32 +0100

    Delete Archive/TP_Final directory

--- Archive/TP_Final/modules/versioning_event_module.py
+++
@@ -1,8 +0,0 @@
-class VersioningEvent:
-    def __init__(self, id, title,rating,cast):
-        
-        self.id= id
-        self.title = title
-        #self.picture = picture
-        self.rating= rating
-        self.cast = cast
